4ba139ee89db838f1bfc5283a9e34c7d84259169
n0name_ <n0name-xd@inbox.ru>
2022-12-15 23:23:57 +0300

    v 1.02 deploy

--- src/helpers/tamplatesTime/createTamplate.ts
+++ src/helpers/tamplatesTime/createTamplate.ts
@@ -1,7 +1,8 @@
 import { createTamplateForTwo } from './createTamplateForTwo';
+import { createTamplateMoreTwo } from './createTamplateMoreTwo';
 
 export const createTamplate = (arrUsers: any) => {
-    if (arrUsers.length === 2) {
-        return createTamplateForTwo();
-    };
+    if (arrUsers.length === 2) return createTamplateForTwo();
+    if (arrUsers.length > 2) return createTamplateMoreTwo(arrUsers);
 };
+
--- src/helpers/tamplatesTime/createTamplateMoreTwo.ts
+++ src/helpers/tamplatesTime/createTamplateMoreTwo.ts
@@ -1,24 +1,24 @@
-export const createTamplateForTwo = () => {
+export const createTamplateMoreTwo = (arrUsers: any) => {
     let result = [];
     let minutes = 0;
+    let positionTimer = 0;
+    let len = arrUsers.length;
 
     for (let i = 0; i < 31; i++) {
-        let positionTimer = 0;
-        positionTimer = i % 2 === 0 ? 0 : 1;
         result.push({
             position: positionTimer,
             time: {
                 from: minutes
             }
         });
-
+         
+        positionTimer += 1;
+        if (positionTimer === len) positionTimer = 0;
         minutes += 2;
     };
-    
+
     return result;
 };
-
-
 
 
 
